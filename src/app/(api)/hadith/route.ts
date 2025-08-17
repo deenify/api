c1f1f46a7b97fd3051bd4e5f9fb23b5ea0ce6a0f
haseeb <haseebarshad1712@gmail.com>
2025-08-17 14:39:36 +0500

    FEAT: Add Hadith route gives metadata response, Add hadith db connection helper

---
+++ src/app/(api)/hadith/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import { methodNotFound } from "@/lib/api/methodNotFound";
+import { getHadithDb } from "@/lib/mongo/connect/connectHadithBookwise";
+
+export async function GET() {
+  try {
+    const db = await getHadithDb();
+    const collection = db.collection("metadata");
+    const data = await collection
+      .find({}, { projection: { _id: 0 } })
+      .toArray();
+
+    if (!data.length) {
+      return NextResponse.json(
+        { error: "Metadata not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(data, {
+      headers: {
+        // "Cache-Control": "public, s-maxage=86400, stale-while-revalidate=3600",
+        // "X-Cache-Note": "24h CDN cache, 1h stale allowed",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching Hadith metadata:", error);
+    return NextResponse.json(
+      { error: `Internal Server Error: ${error}` },
+      { status: 500 }
+    );
+  }
+}
+
+export {
+  methodNotFound as POST,
+  methodNotFound as PUT,
+  methodNotFound as PATCH,
+  methodNotFound as DELETE,
+};
+
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
